620f89837ccfe43bbced9df30c718a239b6368b8
Adin355 <adincaspary@gmail.com>
2023-12-15 10:54:58 -0500

    orgnization

--- Control.py
+++
@@ -1,5 +0,0 @@
-x = 5 
-y = 6 
-print(x+y)
-for i in range (x):
-    print("This is a for loop")
---
+++ src/Control.py
@@ -0,0 +1,8 @@
+
+from gpiozero import Motor
+
+motor1 = Motor(forward = 0, backward = 0)
+
+motor1.forward()
+motor2.backward()
+
