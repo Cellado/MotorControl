945c8eabb38ae7615ab25115b1e16124938494fb
Adin355 <adincaspary@gmail.com>
2023-12-17 12:39:36 -0500

    docstrings

--- src/Control.py
+++ src/Control.py
@@ -1,15 +1,18 @@
 import keyboard
+from time import sleep 
+Gp = True 
 try:
     import RPi.GPIO as GPIO
 except:
     print("Did not import GPIO")
-from time import sleep 
+    GP = False 
+
 def on_key_event(event):
     print (f"you pressed {event}")
 
-    if event.name == 'up':
+    if event.name == 'up up':
         print ('true')
-        
+
 keyboard.on_release(on_key_event)
     
-keyboard.wait('esc')
+keyboard.wait('esc up')
