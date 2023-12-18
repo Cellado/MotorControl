125bd2708e1bee66c45afaeb34d83305af329744
Adin355 <adincaspary@gmail.com>
2023-12-17 20:29:51 -0500

    GPIO cleanup

--- src/Control.py
+++ src/Control.py
@@ -5,6 +5,7 @@ try:
     import RPi.GPIO as GPIO
 except:
     print("Did not import GPIO")
+
     Gp = False 
 # Motor 1
 Motor1F = 11
@@ -14,6 +15,18 @@ Motor1B = 13
 Motor2F = 29 
 Motor2B = 31
 
+try: 
+    GPIO.setmode(GPIO.BOARD)
+    # Motor 1
+    GPIO.setup(Motor1F, GPIO.OUT)
+    GPIO.setup(Motor1B, GPIO.OUT)
+    # Motor 2
+    GPIO.setup(Motor2F, GPIO.OUT)
+    GPIO.setup(Motor2B, GPIO.OUT)
+except:
+    print("GPIO not available")
+
+
 def on_key_event(event):
     """
     Checks for keys being pressed and Prints the key being pressed. 
@@ -22,14 +35,37 @@ def on_key_event(event):
     """
     print (f" you pressed {event}")
     if event.name == "up":
-        print ('true')
+        print('up')
+        try: 
+            GPIO.output(Motor1F, GPIO.HIGH)
+            GPIO.output(Motor2F, GPIO.HIGH)
+        except:
+            print("Tried GPIO up")
+    if event.name == "down":
+        print('down')
+        try: 
+            GPIO.output(Motor1B, GPIO.HIGH)
+            GPIO.output(Motor2B, GPIO.HIGH)
+        except:
+            print("Tried GPIO down")
+
+    if event.name == "space":
+        print('space')
+        try: 
+            GPIO.output(Motor1F, GPIO.LOW)
+            GPIO.output(Motor1B, GPIO.LOW)
+            GPIO.output(Motor2F, GPIO.LOW)
+            GPIO.output(Motor2B, GPIO.LOW)
+        except:
+            print("Tried GPIO space")
+
 keyboard.on_release(on_key_event)  
 keyboard.wait('esc')
-def motorInit():
-    if Gp == True:
-        print("working")
-        GPIO.setmode(GPIO.BOARD)
-        
+
+try: 
+    GPIO.cleanup()
+except: 
+    print("GPIO Clean")
     
 
 
