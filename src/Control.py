db944c1bc9e42af45555016fa90049b1f788b69c
Adin355 <adincaspary@gmail.com>
2024-04-29 21:22:36 -0400

    added pynput

--- src/Control.py
+++ src/Control.py
@@ -1,12 +1,12 @@
-import keyboard
 import time
+import pynput
+
 Gp = True 
 try:
     import RPi.GPIO as GPIO
     print("imported GPIO")
 except:
     print("Did not import GPIO")
-
     Gp = False 
 # Motor 1
 Motor1F = 11
@@ -28,46 +28,10 @@ except:
     print("GPIO not available")
 
 
-def on_key_event(event):
-    """
-    Checks for keys being pressed and Prints the key being pressed. 
-    Parameters: 
-    event: The key being pressed. 
-    """
-    print (f" you pressed {event}")
-    if event.name == "up":
-        print('up')
-        try: 
-            GPIO.output(Motor1F, GPIO.HIGH)
-            GPIO.output(Motor1B, GPIO.LOW)
-            GPIO.output(Motor2F, GPIO.HIGH)
-            GPIO.output(Motor2B, GPIO.LOW)
-            print("GPIO success")
-        except:
-            print("Tried GPIO up")
-    if event.name == "down":
-        print('down')
-        try: 
-            GPIO.output(Motor1B, GPIO.HIGH)
-            GPIO.output(Motor1F, GPIO.LOW)
-            GPIO.output(Motor2B, GPIO.HIGH)
-            GPIO.output(Motor2F, GPIO.LOW)
-        except:
-            print("Tried GPIO down")
-
-    if event.name == "space":
-        print('space')
-        try: 
-            GPIO.output(Motor1F, GPIO.LOW)
-            GPIO.output(Motor1B, GPIO.LOW)
-            GPIO.output(Motor2F, GPIO.LOW)
-            GPIO.output(Motor2B, GPIO.LOW)
-        except:
-            print("Tried GPIO space")
-
-keyboard.on_release(on_key_event)  
-keyboard.wait('esc')
-
+            #GPIO.output(Motor1F, GPIO.HIGH)
+            #GPIO.output(Motor1B, GPIO.LOW)
+            #GPIO.output(Motor2F, GPIO.HIGH)
+            #GPIO.output(Motor2B, GPIO.LOW)
 try: 
     GPIO.cleanup()
 except: 
